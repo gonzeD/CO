0090f534375ddf20d75290c383cba91195e25e4e
dodormeur <damien.1997@hotmail.com>
2016-04-13 17:12:04 +0200

    toujours des tests

--- UCLove/app/src/main/java/com/lsinf/uclove/ConnexionActivity.java
+++ UCLove/app/src/main/java/com/lsinf/uclove/ConnexionActivity.java
@@ -1,13 +1,9 @@
 package com.lsinf.uclove;
 
-import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-//this is a test
-public class ConnexionActivity extends Activity {
+public class ConnexionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
