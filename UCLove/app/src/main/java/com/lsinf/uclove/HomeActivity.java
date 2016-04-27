037f1ab57064d1f7278e2b91789154f0f9efe63a
albanlienart <alban.lienart@student.uclouvain.be>
2016-04-27 23:39:48 +0200

    correction de 2,3 petites erreurs dans HomeActivity

--- UCLove/app/src/main/java/com/lsinf/uclove/HomeActivity.java
+++ UCLove/app/src/main/java/com/lsinf/uclove/HomeActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 /**
  * Created by damien on 20/04/16.
+ * extended by Alban on 27/04/16
  */
 public class HomeActivity extends baseActivity
 {
@@ -19,39 +20,41 @@ public class HomeActivity extends baseActivity
 
         mainUser = new User();
         setContentView(R.layout.home_activity);
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.prenom)).setText(baseActivity.mainUser.getPrenom());
+        if(baseActivity.mainUser.getPrenom() != null)((TextView)findViewById(R.id.prenom)).setText(baseActivity.mainUser.getPrenom());
         //else ((TextView)findViewById(R.id.prenom)).setText("test");
         if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.nom)).setText(baseActivity.mainUser.getNom());
         //else ((TextView)findViewById(R.id.nom)).setText("test");
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.sexe)).setText(baseActivity.mainUser.getSexe());
+        if(baseActivity.mainUser.getSexe() != null)((TextView)findViewById(R.id.sexe)).setText(baseActivity.mainUser.getSexe());
         //else ((TextView)findViewById(R.id.sexe)).setText("test");
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.attirance)).setText(baseActivity.mainUser.getAttirance());
+        if(baseActivity.mainUser.getAttirance() != null)((TextView)findViewById(R.id.attirance)).setText(baseActivity.mainUser.getAttirance());
         //else ((TextView)findViewById(R.id.attirance)).setText("test");
-        /*String tab[] = mainUser.getHobby();
-        String temp = "";
-        for(int i = 0; i<tab.length; i++) temp+=tab[i];
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.hobby)).setText(temp);*/
-        //else ((TextView)findViewById(R.id.hobby)).setText("test");
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.naissance)).setText(baseActivity.mainUser.getNaissance());
+        String tab[] = baseActivity.mainUser.getHobby();
+        String temp = " ";
+        if(tab != null){
+            for(int i = 0; i<tab.length; i++) temp+=tab[i];
+        ((TextView)findViewById(R.id.hobby)).setText(temp);}
+        else ((TextView)findViewById(R.id.hobby)).setText("Liste de mes Hobbys");
+        if(baseActivity.mainUser.getNaissance() != null)((TextView)findViewById(R.id.naissance)).setText(baseActivity.mainUser.getNaissance());
         //else ((TextView)findViewById(R.id.naissance)).setText("test");
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.ville)).setText(baseActivity.mainUser.getVille());
+        if(baseActivity.mainUser.getVille() != null)((TextView)findViewById(R.id.ville)).setText(baseActivity.mainUser.getVille());
         //else ((TextView)findViewById(R.id.ville)).setText("test");
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.mail)).setText(baseActivity.mainUser.getMail());
+        if(baseActivity.mainUser.getMail() != null)((TextView)findViewById(R.id.mail)).setText(baseActivity.mainUser.getMail());
         //else ((TextView)findViewById(R.id.mail)).setText("test");
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.yeux)).setText(baseActivity.mainUser.getYeux());
+        if(baseActivity.mainUser.getYeux() != null)((TextView)findViewById(R.id.yeux)).setText(baseActivity.mainUser.getYeux());
         //else ((TextView)findViewById(R.id.yeux)).setText("test");
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.cheveux)).setText(baseActivity.mainUser.getCheveux());
+        if(baseActivity.mainUser.getCheveux() != null)((TextView)findViewById(R.id.cheveux)).setText(baseActivity.mainUser.getCheveux());
         //else ((TextView)findViewById(R.id.cheveux)).setText("test");
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.tel)).setText(baseActivity.mainUser.getTel());
+        if(baseActivity.mainUser.getTel() != null)((TextView)findViewById(R.id.tel)).setText(baseActivity.mainUser.getTel());
         //else ((TextView)findViewById(R.id.tel)).setText("test");
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.description)).setText(baseActivity.mainUser.getDescription());
+        if(baseActivity.mainUser.getDescription() != null)((TextView)findViewById(R.id.description)).setText(baseActivity.mainUser.getDescription());
         //else ((TextView)findViewById(R.id.description)).setText("test");
-        /*String tab1[] = baseActivity.mainUser.getDisponibilite();
-        String temp1 = "";
+        String tab1[] = baseActivity.mainUser.getDisponibilite();
+        String temp1 = " ";
+        if(tab1 != null){
         for(int i = 0;i<tab1.length;i++)
         {temp1=temp1+tab1[i]+" ";}
-        if(baseActivity.mainUser.getNom() != null)((TextView)findViewById(R.id.dispo)).setText(temp1);*/
-        //else ((TextView)findViewById(R.id.dispo)).setText("test");
+        ((TextView)findViewById(R.id.dispo)).setText(temp1);}
+        else ((TextView)findViewById(R.id.dispo)).setText("Mes Disponibilitées");
 
 
 
