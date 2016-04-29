59f87c18c1f07385d1d42452193e36754e4b72b5
charles <charelsfrys@yahoo.fr>
2016-04-29 17:31:21 +0200

    finish inscription

--- UCLove/app/src/main/java/com/lsinf/uclove/InscriptionActivity.java
+++ UCLove/app/src/main/java/com/lsinf/uclove/InscriptionActivity.java
@@ -2,19 +2,26 @@ package com.lsinf.uclove;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import javax.swing.text.html.ImageView;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 
 /**
  * Created by damien on 13/04/16.
@@ -33,29 +40,30 @@ public class InscriptionActivity extends AppCompatActivity
 
     public void register(View v)
     {
-
+    Log.e("LSINF","test");
      //  "pseudo","password","nom","prenom","sexe","ddnais","mail","tel","ville"};
         DatePicker datePicker = (DatePicker) findViewById(R.id.naissance);
         int day = datePicker.getDayOfMonth();
         int month = datePicker.getMonth() + 1;
         int year = datePicker.getYear();
         String date = day+"/"+month+"/"+year;
+        String sexe =
+                ((RadioButton) (findViewById(((RadioGroup) findViewById(R.id.button_sexe)).getCheckedRadioButtonId()))).getText().toString();
+
 
         new DownloadWebpageTask().execute(
                 ((EditText) findViewById(R.id.pseudo)).getText().toString(),
                 ((EditText) findViewById(R.id.password)).getText().toString(),
                 ((EditText) findViewById(R.id.name)).getText().toString(),
                 ((EditText) findViewById(R.id.firstname)).getText().toString(),
-                ((EditText) findViewById(R.id.sexe)).getText().toString(),
+                sexe,
                 date,
                 ((EditText) findViewById(R.id.mail)).getText().toString(),
                 ((EditText) findViewById(R.id.phone)).getText().toString(),
-                ((EditText) findViewById(R.id.city)).getText().toString(),
+                ((EditText) findViewById(R.id.city)).getText().toString()/*,
                 ((EditText) findViewById(R.id.hobby)).getText().toString(),
                 ((EditText) findViewById(R.id.description)).getText().toString(),
-                ((EditText) findViewById(R.id.langue)).getText().toString(),
-                ((RadioButton) (findViewById(((RadioGroup) findViewById(R.id.button_sexe)).getCheckedRadioButtonId()))).getText().toString()
-        );
+                ((EditText) findViewById(R.id.langue)).getText().toString()*/);
     }
 
     public void takePicture(View v)
@@ -68,15 +76,38 @@ public class InscriptionActivity extends AppCompatActivity
 
     public void onActivityResult (int requestCode, int resultCode, Intent data)
     {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        Log.e("LSINF","result");
+        if (requestCode == 1 && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
+            Log.e("LSINF","result");
             picture = (Bitmap) extras.get("data");
-            ((ImageView)findViewById(R.id.picturePreview)).setImageBitmap(imageBitmap);
+            if(picture == null)Log.e("LSINF","error");
+            ((ImageView)findViewById(R.id.picturePreview)).setImageBitmap(picture);
+        }
+        if(requestCode == 2 && resultCode == RESULT_OK)
+        {
+            Log.e("LSINF","result");
+            Uri selectedImage = data.getData();
+            InputStream imageStream = null;
+            Log.e("LSINF","result");
+            try {
+                imageStream = getContentResolver().openInputStream(selectedImage);
+                Log.e("LSINF","result");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Log.e("LSINF", "result");
+            if(imageStream != null){picture = BitmapFactory.decodeStream(imageStream);
+            ((ImageView)findViewById(R.id.picturePreview)).setImageBitmap(picture);}
+
         }
     }
     public void choosePicture(View v)
     {
-
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 2);
     }
 
     private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
