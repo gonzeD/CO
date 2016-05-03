0c1a193123ae565a6d863a5a126fb1822aa132e2
henryv96 <henry.vanvyve@student.uclouvain.be>
2016-05-03 12:59:28 +0200

    Disponibilités rendez-vous(sans link avec bdd)

--- UCLove/app/src/main/java/com/lsinf/uclove/SettingsActivity.java
+++ UCLove/app/src/main/java/com/lsinf/uclove/SettingsActivity.java
@@ -17,6 +17,10 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 /**
@@ -53,4 +57,39 @@ public class SettingsActivity extends baseActivity
         finish();*/
 
     }
+
+    public void showTimePickerDialog(View v) {
+        TimePickerFragment newFragment = new TimePickerFragment();
+        if(v.getTag().equals("start1"))newFragment.set(0,0);
+        if(v.getTag().equals("start2"))newFragment.set(1,0);
+        if(v.getTag().equals("start3"))newFragment.set(2,0);
+        if(v.getTag().equals("start4"))newFragment.set(3,0);
+        if(v.getTag().equals("start5"))newFragment.set(4,0);
+        if(v.getTag().equals("start6"))newFragment.set(5,0);
+        if(v.getTag().equals("start7"))newFragment.set(6,0);
+        if(v.getTag().equals("end1"))newFragment.set(0,1);
+        if(v.getTag().equals("end2"))newFragment.set(1,1);
+        if(v.getTag().equals("end3"))newFragment.set(2,1);
+        if(v.getTag().equals("end4"))newFragment.set(3,1);
+        if(v.getTag().equals("end5"))newFragment.set(4,1);
+        if(v.getTag().equals("end6"))newFragment.set(5,1);
+        if(v.getTag().equals("end7"))newFragment.set(6,1);
+
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+
+    }
+    String times[] = {"00:00","00:00","00:00","00:00","00:00","00:00","00:00","00:00","00:00","00:00","00:00","00:00","00:00","00:00"};
+    public void setDate(int day,int start,String time)
+    {
+        int ids[] = {R.id.textView1,R.id.textView2,R.id.textView3,R.id.textView4,R.id.textView5,R.id.textView6,R.id.textView7,R.id.textView8
+    ,R.id.textView9,R.id.textView10,R.id.textView11,R.id.textView12,R.id.textView13,R.id.textView14};
+        times[day*2+start] = time;
+        for(int i = 0;i<14;i++)
+        {
+            ((TextView)findViewById(ids[i])).setText(times[i]);
+        }
+
+
+    }
 }
+
